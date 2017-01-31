11aab4a27425040087da250db5a06648068755fa
Yang Guan <yangguan@google.com>
2017-01-31 18:26:32 +0000

    Sample for using watches on Pods

---
+++ watch/main.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+
+	"github.com/lookuptable/client-go-example/client"
+
+	"k8s.io/client-go/pkg/api/v1"
+)
+
+func main() {
+	flag.Parse()
+
+	clientset, err := client.NewClientSet()
+	if err != nil {
+		panic(err.Error())
+	}
+
+	w, err := clientset.CoreV1().Pods("").Watch(v1.ListOptions{})
+	if err != nil {
+		panic(err.Error())
+	}
+
+	c := w.ResultChan()
+	for {
+		event := <-c
+		fmt.Println(event)
+	}
+}
